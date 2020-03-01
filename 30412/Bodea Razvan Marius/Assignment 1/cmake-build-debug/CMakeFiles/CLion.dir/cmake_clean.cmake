c2417ef22802adb3c7ad94b8253565d8b6740953
Bodea Razvan-Marius <razvan.bodea2700@gmail.com>
2020-03-01 22:47:15 +0200

    Assignment 1

---
+++ 30412/Bodea Razvan Marius/Assignment 1/cmake-build-debug/CMakeFiles/CLion.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CLion.exe"
+  "CLion.exe.manifest"
+  "CLion.pdb"
+  "CMakeFiles/CLion.dir/main.c.obj"
+  "CMakeFiles/CLion.dir/sll.c.obj"
+  "libCLion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
