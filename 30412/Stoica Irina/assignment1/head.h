65c73d291be600e48d6c4823a5b5fcdb62c3a1cb
Irina Stoica <Irina.STOICA@student.utcluj.ro>
2020-03-03 15:56:09 +0200

    assignment 1

---
+++ 30412/Stoica Irina/assignment1/head.h
@@ -0,0 +1,15 @@
+//
+// Created by Irina on 02/03/2020.
+//
+
+#ifndef ASSIGNMENT1_HEAD_H
+#define ASSIGNMENT1_HEAD_H
+
+typedef struct node{
+    int number;
+    struct node * next;
+}node;
+
+node * first, *last;
+
+#endif //ASSIGNMENT1_HEAD_H
