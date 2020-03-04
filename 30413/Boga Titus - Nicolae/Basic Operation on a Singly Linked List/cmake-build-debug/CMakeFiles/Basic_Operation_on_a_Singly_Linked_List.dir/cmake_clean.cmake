e7f9d63b15b3db7ed30554a78db24b04523d50e3
titusboga <35780770+titusboga@users.noreply.github.com>
2020-03-04 14:50:18 +0200

    First

---
+++ 30413/Boga Titus - Nicolae/Basic Operation on a Singly Linked List/cmake-build-debug/CMakeFiles/Basic_Operation_on_a_Singly_Linked_List.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Basic_Operation_on_a_Singly_Linked_List.exe"
+  "Basic_Operation_on_a_Singly_Linked_List.exe.manifest"
+  "Basic_Operation_on_a_Singly_Linked_List.pdb"
+  "CMakeFiles/Basic_Operation_on_a_Singly_Linked_List.dir/main.c.obj"
+  "libBasic_Operation_on_a_Singly_Linked_List.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Basic_Operation_on_a_Singly_Linked_List.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
